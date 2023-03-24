502014b000417e3517c1c991ac767624b552b3eb
PERPELEX <hannanamir18@gmail.com>
2023-03-24 18:10:07 +0500

    Alhamdulilah
    I finally added java to this repoisitory

---
+++ src/Finaly.java
@@ -0,0 +1,5 @@
+public class Finaly {
+    public static void second(){
+        System.out.println("Finally i am looking at executable java code!");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Alhamdullilah!Hello world!");
+        Finaly obj=new Finaly();
+        obj.second();
+    }
+}
